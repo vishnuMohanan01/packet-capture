88739074b2331eabd2b92c88ce62e2bead64a829
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-09 22:38:17 +0530

    tests

--- src/pcapture/sniffer.py
+++ src/pcapture/sniffer.py
@@ -3,7 +3,7 @@ from scapy.sendrecv import AsyncSniffer
 from custom_session import generate_session_class
 
 
-def create_sniffer(input_interface, sys_dst_ip):
+def create_sniffer(input_interface, sys_dst_ip, cb=None):
     assert (input_interface is None)
 
     custom_session = generate_session_class(sys_dst_ip)
@@ -11,7 +11,7 @@ def create_sniffer(input_interface, sys_dst_ip):
     return AsyncSniffer(
         iface=input_interface,
         filter="ip and (tcp or udp)",
-        prn=None,
+        # prn=cb,
         session=custom_session,
         store=False,
         count=0
@@ -23,8 +23,14 @@ def main():
     input_interface = None
     sys_dst_ip = None
 
+    # TEST: this is a simple callback function
+    def simple_cb(foo, bar):
+        print(f"Simple CB happened. Args: {foo}, {bar}")
+        pass
+
     sniffer = create_sniffer(
-        input_interface,
+        # cb=simple_cb,
+        input_interface=input_interface,
         sys_dst_ip=sys_dst_ip
     )
 
