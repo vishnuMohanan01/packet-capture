1e26c4f94e05e6045dfd2580a93804a8cec65e5d
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-09 12:36:38 +0530

    replaced argparse vars with local vars

--- src/pcapture/sniffer.py
+++ src/pcapture/sniffer.py
@@ -1,5 +1,3 @@
-import argparse
-
 from scapy.sendrecv import AsyncSniffer
 
 from .flow_session import generate_session_class
@@ -18,62 +16,21 @@ def create_sniffer(
         prn=None,
         session=new_flow_session,
         store=False,
+        count=0
     )
 
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    input_group = parser.add_mutually_exclusive_group(required=True)
-    input_group.add_argument(
-        "-i",
-        "--interface",
-        action="store",
-        dest="input_interface",
-        help="capture online data from INPUT_INTERFACE",
-    )
-
-    input_group.add_argument(
-        "-f",
-        "--file",
-        action="store",
-        dest="input_file",
-        help="capture offline data from INPUT_FILE",
-    )
-
-    output_group = parser.add_mutually_exclusive_group(required=False)
-    output_group.add_argument(
-        "-c",
-        "--csv",
-        "--flow",
-        action="store_const",
-        const="flow",
-        dest="output_mode",
-        help="output flows as csv",
-    )
-
-    url_model = parser.add_mutually_exclusive_group(required=False)
-    url_model.add_argument(
-        "-u",
-        "--url",
-        action="store",
-        dest="url_model",
-        help="URL endpoint for send to Machine Learning Model. e.g http://0.0.0.0:80/prediction",
-    )
-
-    parser.add_argument(
-        "output",
-        help="output file name (in flow mode) or directory (in sequence mode)",
-    )
-
-    args = parser.parse_args()
+    input_interface = None
+    output_mode = None
+    output = None
+    url_model = None
 
     sniffer = create_sniffer(
-        args.input_file,
-        args.input_interface,
-        args.output_mode,
-        args.output,
-        args.url_model,
+        input_interface,
+        output_mode,
+        output,
+        url_model,
     )
     sniffer.start()
 
