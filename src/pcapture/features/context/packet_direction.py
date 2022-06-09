665e5dd27f2e52360ff2bca309930b2af57afbaa
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-09 14:57:26 +0530

    combined context files

--- src/pcapture/features/context/packet_direction.py
+++ src/pcapture/features/context/packet_direction.py
@@ -3,13 +3,67 @@
 from enum import Enum, auto
 
 
-class PacketDirection(Enum):
-    """Packet Direction creates constants for the direction of the packets.
+class PacketDirection:
+    def __init__(self, packet, system_dst_ip):
+        self.system_dst_ip = system_dst_ip
+        self.packet = packet
 
-    There are two given directions that the packets can Feature along
-    the line. PacketDirection is an enumeration with the values
-    forward (1) and reverse (2).
-    """
+    class Direction(Enum):
+        """Packet Direction creates constants for the direction of the packets.
 
-    FORWARD = auto()
-    REVERSE = auto()
+        There are two given directions that the packets can Feature along
+        the line. PacketDirection is an enumeration with the values
+        forward (1) and reverse (2).
+        """
+
+        REVERSE = auto()
+        FORWARD = auto()
+
+    def get_direction(self):
+        """Return Packet's inbound or outbound direction
+        """
+
+        if self.system_dst_ip == self.packet["IP"].dst:
+            return self.Direction.FORWARD
+        else:
+            return self.Direction.REVERSE
+
+    def get_packet_flow_key(self) -> tuple:
+        """Creates a key signature for a packet.
+
+        Summary:
+            Creates a key signature for a packet so it can be
+            assigned to a flow.
+
+        Args:
+            packet: A network packet
+            direction: The direction of a packet
+
+        Returns:
+            A tuple of the String IPv4 addresses of the destination,
+            the source port as an int,
+            the time to live value,
+            the window size, and
+            TCP flags.
+
+        """
+        if "TCP" in self.packet:
+            protocol = "TCP"
+        elif "UDP" in self.packet:
+            protocol = "UDP"
+        else:
+            raise Exception("Only TCP protocols are supported.")
+
+        if self.get_direction() == self.Direction.FORWARD:
+            dest_ip = self.packet["IP"].dst
+            src_ip = self.packet["IP"].src
+            src_port = self.packet[protocol].sport
+            dest_port = self.packet[protocol].dport
+        else:
+            dest_ip = self.packet["IP"].src
+            src_ip = self.packet["IP"].dst
+            src_port = self.packet[protocol].dport
+            dest_port = self.packet[protocol].sport
+
+        return dest_ip, src_ip, src_port, dest_port
+
